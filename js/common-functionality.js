b85ee75a389392e90925831dac92374a21ab3e4f
Davis McPhee <davismcphee@hotmail.com>
2017-02-09 02:01:03 -0400

    Nav fix 2
    anothr nav fix

--- js/common-functionality.js
+++ js/common-functionality.js
@@ -4,9 +4,18 @@ var header = document.getElementsByTagName("header");
 var colourSelector = new ColourSelector(colours, navMainLinks, header);
 var navMain = document.getElementsByClassName("nav-main")[0];
 
-window.addEventListener("resize", function(e) {
+console.log(document.documentElement.clientWidth);
+
+function adjustNavBar() {
     if (document.documentElement.clientWidth < 370)
+        screen
         navMain.classList.add("screen-small");
     else
         navMain.classList.remove("screen-small");
+}
+
+adjustNavBar();
+
+window.addEventListener("resize", function(e) {
+    adjustNavBar();
 });
